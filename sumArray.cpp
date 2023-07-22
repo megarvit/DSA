f4628d12626cf219544ef045e05041ea5d2f5187
megarvit <garvitgarvitjain0306@gmail.com>
2023-07-22 11:37:58 +0530

    sum of an array

--- sumArray.cpp
+++ sumArray.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
 
+// int sumarr(int arr[],int n){
+//     int sum=0;
+//     for(int i=0;i<n;i++){
+//         sum+=arr[i];
+//     }
+//     return sum;
+// }
+// int main(){
+//     int a[100];
+//     int n;
+
+//     cin>>n;
+//     for(int i=0;i<n;i++){
+//         cin>>a[i];
+//     }
+//     cout<<"sum of array"<<sumarr(a,n)<<endl
+// }
 int SumArr(int arr[],int n){
     int sum=0;
     for(int i=0;i<n;i++){
