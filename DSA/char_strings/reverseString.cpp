2403727a2e006268e4d128c518367114723850fd
megarvit <garvitgarvitjain0306@gmail.com>
2023-09-15 13:51:05 +0530

    DSA

--- DSA/char_strings/reverseString.cpp
+++ DSA/char_strings/reverseString.cpp
@@ -2,3 +2,42 @@
 #include<vector>
 using namespace std;
 
+void reverse(char name[],int n){
+
+    int s=0;
+    int e= n-1;
+
+    while(s<e){
+        swap(name[s++],name[e--]);
+    }
+}
+
+int getlength(char name[]){
+    int count = 0;
+    for(int i=0; name[i]!='\0';i++){
+        count++;
+    }
+    return count;
+}
+
+
+int main(){
+    char name[20];
+    
+    cout<<" enter your name: "<<endl;
+    cin>>name;
+
+    cout<< "your name is ";
+    cout << name <<endl;
+
+    int len = getlength(name);
+
+    cout<<" length is "<<len<<endl;;
+
+    reverse(name,len);
+
+    cout<<"reverse is " << name<<endl;
+    
+    return 0;
+    
+}
