f28e3c30f44534398ada453743f0c5fa7655bf35
megarvit <garvitgarvitjain0306@gmail.com>
2023-09-17 19:06:25 +0530

    DSA

---
+++ DSA/lec22/compressedstring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i=0;
+        int ansIndex = 0;
+        int n = chars.size();
+
+        while(i<n){
+
+            int j=i+1;
+            while(j<n && chars[i] == chars[j]){
+                j++;
+            }
+            //yaha kab aooge
+            //ya toh vector poora traverse kargaya 
+            //ya fir new/diff character encounter kia hai
+
+            chars[ansIndex++] = chars[i];
+
+            int count = j - i;
+
+            //converting count int to string
+            //converting count into single digit and saving in answer
+
+            if(count>1){
+                string cnt = to_string(count);
+                for(char ch: cnt){
+                    chars[ansIndex++] = ch;
+                }
+            }
+            i=j;
+        }
+        return ansIndex;
+    }
+};
