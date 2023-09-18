fc982633a4a5929ef01ea38a65510e9e42a68205
megarvit <garvitgarvitjain0306@gmail.com>
2023-09-18 23:06:01 +0530

    DSA

---
+++ DSA/lec23/2darray.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //2d array
+    //int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    //int arr[3][4]={{1,11,111,1111},{2,22,222,2222},{3,33,333,3333}};
+    int arr[3][4];
+
+    //taking input row wise input i row j is column
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    /*
+   //taking input column wise input i is column here and j is row
+    for(int i=0;i<4;i++){
+        for(int j=0;j<3;j++){
+            cin>>arr[j][i];
+        }
+    }
+    */
+
+    //print
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
