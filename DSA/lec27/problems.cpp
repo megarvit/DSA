c578fe26bcac9e360f60eaa33e6c5b7e1ec20af7
megarvit <garvitgarvitjain0306@gmail.com>
2023-09-23 19:59:12 +0530

    DSA

---
+++ DSA/lec27/problems.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int f = 8;
+    // int *p = &f;
+    // cout<<(*p)++<<" ";
+    // cout<<f<<endl;
+
+    
+
+
+
+
+    return 0;
+}
