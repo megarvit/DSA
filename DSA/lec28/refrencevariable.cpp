04245a8a42e8ad83cb29c59c94aeebc93c309ab7
megarvit <garvitgarvitjain0306@gmail.com>
2023-09-25 20:57:15 +0530

    DSA

--- DSA/lec28/refrencevariable.cpp
+++ DSA/lec28/refrencevariable.cpp
@@ -1,17 +1,32 @@
 #include<iostream>
 using namespace std;
 
+void update2(int& n){
+    n++;
+}
+
+void update(int n){
+    n++;
+}
+
 int main(){
 
-    int i=5;
-    int& j = i; //refrence variable
+    // int i=5;
+    // int& j = i; //refrence variable
 
-    cout<<i<<endl;
-    i++;
-    cout<<i<<endl;
-    j++;
-    cout<<i<<endl;
-    cout<<j<<endl;
+    // cout<<i<<endl;
+    // i++;
+    // cout<<i<<endl;
+    // j++;
+    // cout<<i<<endl;
+    // cout<<j<<endl;
+
+    int n=5;
+    cout<<"before: "<<n<<endl;
+    // update(n);
+    update2(n);
+    cout<<"after: "<<n<<endl;
+
 
 
     return 0;
