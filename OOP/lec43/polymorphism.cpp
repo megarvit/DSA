5efa6ccdadb5e2a63b1907fe085e4130e626378f
megarvit <garvitgarvitjain0306@gmail.com>
2023-10-17 23:57:02 +0530

    DSA

--- OOP/lec43/polymorphism.cpp
+++ OOP/lec43/polymorphism.cpp
@@ -13,11 +13,64 @@ class A {
     }
 };
 
+class B{
+    public:
+    int a;
+    int b;
+
+    public:
+    int add(){
+        return a+b;
+    }
+
+    void operator+ (B &obj){
+        // int value1 = this->a;
+        // int value2 = obj.a;
+        // cout<< "output "<<value2-value1<<endl;
+
+        cout<<"hello beta"<<endl;
+    }
+
+    void operator() (){
+        cout<<"m bracket hun bhaiya"<<this->a<<endl;
+    }
+};
+
+class animal{
+    public:
+    void speak(){
+        cout<<"speaking "<<endl;
+    }
+};
+
+class dog: public animal{
+    public:
+    void speak(){
+        cout<<"barking "<<endl;
+    }
+};
+
 
 int main(){
 
-    A obj;
-    obj.sayHello();
+    dog obj;
+    obj.speak();
+
+/*
+    B obj1,obj2;
+
+    obj1.a = 4;
+    obj2.a = 7;
+
+    obj1 + obj2;
+
+    obj1();
+*/
+
+
+
+    // A obj;
+    // obj.sayHello();
   
 
     return 0;
