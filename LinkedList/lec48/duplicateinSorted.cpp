4713690c2fa504f8d20d8b8609c9e9c1554daf5e
megarvit <garvitgarvitjain0306@gmail.com>
2023-10-29 18:03:14 +0530

    DSA

--- LinkedList/lec48/duplicateinSorted.cpp
+++ LinkedList/lec48/duplicateinSorted.cpp
@@ -1,28 +1,29 @@
-/**
- * Definition of doubly linked list:
- * class Node {
- * public:
- *      int data;
- *      Node *prev;
- *      Node *next;
- *      Node() {
- *          this->data = 0;
- *          this->prev = NULL;
- *          this->next = NULL;
- *      }
- *      Node(int data) {
- *          this->data = data;
- *          this->prev = NULL;
- *          this->next = NULL;
- *      }
- *      Node (int data, Node *next, Node *prev) {
- *          this->data = data;
- *          this->prev = prev;
- *          this->next = next;
- *      }
- * };
- *
- *************************************************************************/
+#include<iostream>
+#include<map>
+using namespace std;
+
+  class Node {
+  public:
+       int data;
+       Node *prev;
+       Node *next;
+       Node() {
+           this->data = 0;
+           this->prev = NULL;
+           this->next = NULL;
+       }
+       Node(int data) {
+           this->data = data;
+           this->prev = NULL;
+           this->next = NULL;
+       }
+       Node (int data, Node *next, Node *prev) {
+           this->data = data;
+           this->prev = prev;
+           this->next = next;
+       }
+  };
+
 
 Node * removeDuplicates(Node *head)
 {
