79412313fb8a26ea8fce8859266b2da194e13269
megarvit <garvitgarvitjain0306@gmail.com>
2023-11-05 10:28:47 +0530

    DSA

---
+++ Stack/lec52/reverseStack.cpp
@@ -0,0 +1,31 @@
+// void insertAtBottom(stack<int> &stack, int element){
+//     // base case
+//     if(stack.empty()){
+//         stack.push(element);
+//         return ;
+//     }
+
+//     int num = stack.top();
+//     stack.pop();
+
+//     //recursive call
+//     insertAtBottom(stack,element);
+
+//     stack.push(num);
+// }
+
+
+// void reverseStack(stack<int> &stack) {
+//     // base case
+//     if(stack.empty()){
+//         return ;
+//     }
+
+//     int num = stack.top();
+//     stack.pop();
+
+//     //recursive call
+//     reverseStack(stack);
+
+//     insertAtBottom(stack, num);
+// }
