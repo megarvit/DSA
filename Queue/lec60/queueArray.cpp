7ee6aa040e7419dd34f92ff50b854a4cc202a16e
megarvit <garvitgarvitjain0306@gmail.com>
2023-11-29 22:14:35 +0530

    DSA

--- Queue/lec60/queueArray.cpp
+++ Queue/lec60/queueArray.cpp
@@ -1,10 +1,60 @@
-#include<iostream>
-#include<queue>
-using namespace std;
+#include <bits/stdc++.h> 
+class Queue {
+    int* arr;
+    int qfront;
+    int rear;
+    int size;
 
-int main(){
+public:
+    Queue() {
+        size = 10001;
+        arr = new int[size];
+        qfront = 0;
+        rear = 0;
+    }
 
+    /*----------------- Public Functions of Queue -----------------*/
 
+    bool isEmpty() {
+        if(qfront == rear){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
-    return 0;
-}
+    void enqueue(int data) {
+        if(rear == size)
+            // cout<<"Queue is full"<<endl;
+            return ;
+        else
+        {
+            arr[rear++] = data;
+        }
+    }
+
+    int dequeue() {
+        if(qfront == rear)
+            return -1;
+        else
+        {
+            int ans = arr[qfront];
+            arr[qfront++] = -1;
+            if(qfront == rear){
+                qfront = 0;
+                rear = 0;
+            }
+            return ans;
+        }
+    }   
+
+    int front() {
+        if(qfront == rear)
+            return -1;
+        else
+        {
+            return arr[qfront];
+        }
+    }
+};
